e0695a4a203adbed07378cc7185664240d8f7603
floatpigeon <floatpigeon@proton.me>
2024-11-02 22:02:41 +0800

    Fix:Astar

--- include/Node.hpp
+++ include/Node.hpp
@@ -2,24 +2,22 @@
 
 #include <utility>
 class Node {
-public:
-  int x, y;
-  float value_g, value_h;
-  Node *parent;
+   public:
+    int x, y;
+    float value_g, value_h;
+    Node* parent;
 
-  Node(int x, int y, int g = 0, int h = 0, Node *parent = nullptr)
-      : x(x), y(y), value_g(g), value_h(h), parent(parent) {}
+    Node(int x, int y, int g = 0, int h = 0, Node* parent = nullptr)
+        : x(x), y(y), value_g(g), value_h(h), parent(parent) {}
 
-  std::pair<int, int> position() {
-    std::pair<int, int> position;
-    position.first = x;
-    position.second = y;
-    return position;
-  }
+    std::pair<int, int> position() {
+        std::pair<int, int> position;
+        position.first = x;
+        position.second = y;
+        return position;
+    }
 
-  float value_f() const { return value_h + value_g; }
+    float value_f() const { return value_h + value_g; }
 
-  bool operator>(const Node &other) const {
-    return value_f() > other.value_f();
-  }
+    bool operator>(const Node& other) const { return value_f() > other.value_f(); }
 };
--- src/Astar.cpp
+++ src/Astar.cpp
@@ -1,67 +1,62 @@
 #include "Astar.hpp"
-#include "Node.hpp"
-#include "map.hpp"
+
 #include <cstddef>
 #include <iostream>
 #include <queue>
 #include <utility>
 #include <vector>
 
-std::vector<std::pair<int, int>> Astar::getpath(std::pair<int, int> begin,
-                                                std::pair<int, int> end) {
-  std::vector<std::pair<int, int>> direction = {
-      {1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+#include "Node.hpp"
+#include "map.hpp"
 
-  Node *beginNode =
-      new Node(begin.first, begin.second, 0, calc_h(begin, end), nullptr);
+std::vector<std::pair<int, int>> Astar::getpath(std::pair<int, int> begin, std::pair<int, int> end) {
+    std::vector<std::pair<int, int>> direction = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
 
-  OpenList.push(*beginNode);
-  changeState(*beginNode, CLOSED);
+    Node* beginNode = new Node(begin.first, begin.second, 0, calc_h(begin, end), nullptr);
 
-  while (!OpenList.empty()) {
-    Node currentNode = OpenList.top();
-    OpenList.pop();
-    changeState(currentNode, CLOSED);
+    OpenList.push(*beginNode);
+    changeState(*beginNode, CLOSED);
 
-    if (currentNode.x == end.first && currentNode.y == end.second) {
-      std::vector<std::pair<int, int>> path;
-      Node *node = new Node(currentNode);
-      std::cout << "result" << std::endl;
+    while (!OpenList.empty()) {
+        Node currentNode = OpenList.top();
+        OpenList.pop();
+        changeState(currentNode, CLOSED);
 
-      while (node != nullptr) {
-        path.emplace_back(node->position());
-        changeState(*node, PATH);
-        auto ptr = node;
-        node = node->parent;
-        delete ptr;
-      }
-      delete node;
-      return path;
+        if (currentNode.x == end.first && currentNode.y == end.second) {
+            std::vector<std::pair<int, int>> path;
+            Node* node = new Node(currentNode);
+            std::cout << "result" << std::endl;
+
+            while (node != nullptr) {
+                path.emplace_back(node->position());
+                changeState(*node, PATH);
+                auto ptr = node;
+                node = node->parent;
+                delete ptr;
+            }
+            delete node;
+            return path;
+        }
+
+        for (size_t i = 0; i < direction.size(); i++) {
+            std::pair<int, int> next;
+            next.first = currentNode.x + direction[i].first;
+            next.second = currentNode.y + direction[i].second;
+
+            if (next.first < 0 || next.first >= row || next.second < 0 || next.second >= col)
+                continue;
+            if (Astar_map[next.first][next.second] != EMPTY)
+                continue;
+
+            Node* parent = new Node(currentNode);
+            Node* openNode = new Node(next.first, next.second, calc_g(next.first, next.second, parent->x, parent->y),
+                                      calc_h(next, end), parent);
+
+            OpenList.push(*openNode);
+            changeState(*openNode, OPEND);
+
+            delete openNode;
+        }
     }
-
-    for (size_t i = 0; i < direction.size(); i++) {
-
-      std::pair<int, int> next;
-      next.first = currentNode.x + direction[i].first;
-      next.second = currentNode.y + direction[i].second;
-
-      if (next.first < 0 || next.first >= row || next.second < 0 ||
-          next.second >= col)
-        continue;
-      if (Astar_map[next.first][next.second] != EMPTY)
-        continue;
-
-      Node *parent = new Node(currentNode);
-      Node *openNode =
-          new Node(next.first, next.second,
-                   calc_g(next.first, next.second, parent->x, parent->y),
-                   calc_h(next, end), parent);
-
-      OpenList.push(*openNode);
-      changeState(*openNode, OPEND);
-
-      delete openNode;
-    }
-  }
-  return {};
+    return {};
 }
